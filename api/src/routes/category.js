5f37128464ade05f9c5e43c885e7f0c76a271e06
Sergio Pepe <sergio_pp95@hotmail.com>
2020-11-03 23:36:48 -0300

    delete and put category

--- api/src/routes/category.js
+++ api/src/routes/category.js
@@ -5,7 +5,25 @@ const { Op } = require('sequelize');
 
 category.post('/category', (req, res , next) => {
     const request = req.body
+    
     Category.create(request)
     .then((cat) => res.send(cat)) 
 })
 
+
+category.delete('category/:id', (req, res, next) => {
+    const { id } = req.params
+
+    Category.findOne({where: {id}})
+    .then(cat => {
+        cat.destroy()
+    })
+})
+
+category.put('category/:id', (req, res, next) => {
+    const { id } = req.params;
+
+    Category.findOne({where: {id}})
+    .then(cat => res.send(cat))
+    .catch(next);    
+})

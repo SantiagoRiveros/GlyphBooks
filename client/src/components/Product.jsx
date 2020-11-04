4f55442d6cb9177b25eb338e12a1a0e111f8034d
Mauricio Ocando <mauroocando@gmail.com>
2020-11-04 11:50:13 -0300

    homepage + components css

---
+++ client/src/components/Product.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import Portada from "../Images/poretada1.jpg"
+import "../Sccs/Product.scss"
+
+export default function Product(){
+
+    return(
+        <div className="Producto">
+            <div className="Center">
+            <img className="Libroimg"src={Portada}/>
+            <div className="description">
+            <h2 className="title">Harry potter 23</h2>
+            <h3 className="Price">Precio: 19.99</h3>
+            <h4 className="Stock">Stock: 23</h4>
+            </div>
+            </div>
+        </div>
+    )
+}

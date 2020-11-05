5da2ca2bf73f9763175f3d92b89370c65257e194
Santiago Riveros <santiagoriverosjasis@gmail.com>
2020-11-05 17:17:32 -0300

    sidebar fix

---
+++ client/src/Components/Catalogo/Sidebar.jsx
@@ -0,0 +1,23 @@
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
+
+export default({setCategorias})=>{
+    useEffect(() => {
+        axios.get("http://localhost:3000/category")
+        .then(data => {setCategorias(data)})
+    }, []);
+    //HandleClick muestra donde se hizo click, entonces OnClick=HandleClick(category.name)
+    const HandleClick = ((event)=>{
+        setCategorias(event.target.innerHTML);
+    });
+    return(
+        <div>
+            <h1>CATEGORIAS</h1>
+            <ul>
+                {props.category.map((category)=>{
+                    return <li OnClick={HandleClick}>{category.name}</li>
+                })}
+            </ul>
+        </div>
+    )
+}

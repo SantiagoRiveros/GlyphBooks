01ef71c0986ca4e214879c1d0584a358161ca70b
Santiago Riveros <santiagoriverosjasis@gmail.com>
2020-11-05 12:16:15 -0300

    sidebar funcional

--- client/src/Components/Sidebar.js
+++ client/src/Components/Sidebar.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
-export default(props)=>{
+export default({setCategorias})=>{
+    useEffect(() => {
+        axios.get("http://localhost:3000/category")
+        .then(data => {setCategorias(data)})
+    })
+    //HandleClick muestra donde se hizo click, entonces OnClick=HandleClick(category.name)
+    HandleClick((event)=>{
+        setCategorias(event.target.innerHTML);
+    }, [])
     return(
         <div>
             <h1>CATEGORIAS</h1>
             <ul>
                 {props.category.map((category)=>{
-                    return <li>{category.name}</li>
+                    return <li OnClick={HandleClick}>{category.name}</li>
                 })}
             </ul>
         </div>
--- client/src/components/Catalogue.jsx
+++ client/src/components/Catalogue.jsx
@@ -4,13 +4,14 @@ import Sidebar from "./Sidebar.js"
 
 export default function Catalogue(){
     const [productos, setProductos] = useState([]);
+    const [categorias, setCategorias] = useState([]);
     useEffect(() => {
         axios.get("http://localhost:3000/products")
         .then(data => {setProductos(data)})
-    })
+    }, [])
     return(
         <div>
-            <Sidebar/>
+            <Sidebar setCategorias={setCategorias}/>
            {productos && productos.map((producto) => {
                return <Producto img={producto.img} title={producto.title} price={producto.price}/>
            })}

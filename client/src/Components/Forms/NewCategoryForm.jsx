6835e2ac35420a6f05f1f44f6a986feb1d84181c
shiberus <santiagolopezcane@gmail.com>
2020-11-06 11:58:27 -0300

    crud funciona

--- client/src/Components/Forms/NewCategoryForm.jsx
+++ client/src/Components/Forms/NewCategoryForm.jsx
@@ -1,38 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
+export default function NewForm() {
+  const [input, setInput] = useState({
+    name: "",
+    description: "",
+  });
 
-export default function NewForm () {
-    const [input, setInput] = useState({
-        name: '',
-        description: '',
-    })
+  const handleChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const handleInputSubmit = (e) => {
-        setInput({
-            ...input,
-            [e.target.name] : e.target.value
-        });
-    }
-    
-    const handleSubmit = (e) => {
-    alert('Category was submitted: ' + e.target.value);
+  const handleSubmit = (e) => {
+    alert("Category was submitted: " + e.target.value);
     e.preventDefault();
-    }
-    
-        return (
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Name:
-                    <input type="text" value={name} name ='name' onChange={handleInputSubmit}/>
-                </label>
-                <label>
-                    description:
-                    <input type="text" value={description} name ='description' onChange={handleInputSubmit}/>
-                </label>
-                <input type="submit" value="Submit" />
-            </form>
-        ); 
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          value={input.name}
+          name="name"
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        description:
+        <input
+          type="text"
+          value={input.description}
+          name="description"
+          onChange={handleChange}
+        />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
 }
-
-
-

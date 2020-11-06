e4575e182ab8357b4aa0210acf325d453ebd1ed8
Sebastian Herrera <sebastian.herrera@tixo.com>
2020-11-06 15:11:36 -0300

    'Css-Styles-And-Minor-Necessary-Changes'

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 //componentes
-import NavBar from "./components/NavBar";
-import Homepage from "./components/Home";
+import NavBar from "./Components/NavBar";
+import Homepage from "./Components/Home";
 import Carrito from "./routing/Carrito";
 import Ingresar from "./routing/Ingresar";
 import Faq from "./routing/faq.jsx";
-import Catalogo from "./components/Catalogo/Catalogue";
-import Crud from "./components/Forms/CrudProductForm";
+import Catalogo from "./Components/Catalogo/Catalogue";
+import Crud from "./Components/Forms/CrudProductForm";
 
 function App() {
   const [producto, setProducto] = useState(null);

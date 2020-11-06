b3beb3ae92a0a99797582d7b08eb62ff15787b8b
Santiago Riveros <santiagoriverosjasis@gmail.com>
2020-11-06 10:55:53 -0300

    los elementos pushean

--- client/src/Components/Catalogo/Catalogue.jsx
+++ client/src/Components/Catalogo/Catalogue.jsx
@@ -1,30 +1,35 @@
-import React, {useEffect, useState} from 'react';
-import Producto from "./productCard.jsx"
+import React, { useEffect, useState } from "react";
+import Producto from "./productCard.jsx";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 
-import axios from 'axios'
-
-export default function Catalogue(){
-    const [productos, setProductos] = useState([]);
-    const [categorias, setCategorias] = useState([]);
-    useEffect(() => {
-        axios.get("http://localhost:3000/products")
-        .then(data => {
-            console.log(data)
-            setProductos(data.data)
-        })
-        .catch(err => console.log(err))
-    }, [])
-    return(
-        <div>
-            {/* <Sidebar setCategorias={setCategorias}/> */}
-           {productos.length && productos.map((producto) => {
-               return <Producto 
-                img={producto.img} 
-                title={producto.title} 
-                price={producto.price}
-                />
-           })}
-        </div>
-         
-    )
+export default function Catalogue() {
+  const [productos, setProductos] = useState([]);
+  const [categorias, setCategorias] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/products")
+      .then((data) => {
+        console.log(data);
+        setProductos(data.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  const { push } = useHistory();
+  return (
+    <div>
+      <Sidebar setCategorias={setCategorias} />
+      {productos.length &&
+        productos.map((producto) => {
+          return (
+            <Producto
+              img={producto.img}
+              title={producto.title}
+              price={producto.price}
+              OnClick={() => push(`/productos/${producto.id}`)}
+            />
+          );
+        })}
+    </div>
+  );
 }

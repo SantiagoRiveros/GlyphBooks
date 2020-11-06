38a929074414f9a2f7ab4f9d75cd7cb7f8dfe7c5
Santiago Riveros <santiagoriverosjasis@gmail.com>
2020-11-06 16:28:38 -0300

    aca esta el horrible agregador de categorias

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,13 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 //componentes
 
-import NavBar from "./Components/NavBar";
-import Homepage from "./Components/Home";
+import NavBar from "./components/NavBar";
+import Homepage from "./components/Home";
 import Carrito from "./routing/Carrito";
 import Ingresar from "./routing/Ingresar";
 import Faq from "./routing/faq.jsx";
-import Catalogo from "./Components/Catalogo/Catalogue";
-import Crud from "./Components/Forms/CrudProductForm";
+import Catalogo from "./components/Catalogo/Catalogue";
+import Crud from "./components/Forms/CrudProductForm";
 
 function App() {
   const [producto, setProducto] = useState(null);

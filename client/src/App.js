6f9324e224e03e7f78becbf7e6f9e0520bc25782
Mauricio Ocando <mauroocando@gmail.com>
2020-11-10 11:02:26 -0300

    estilos faq

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import NavBar from "./components/NavBar";
 import Homepage from "./components/Home";
 import Carrito from "./routing/Carrito";
 import Ingresar from "./routing/Ingresar";
-import Faq from "./routing/faq.jsx";
+import Faq from "./components/Faq";
 import Catalogo from "./components/Catalogo/Catalogue";
 import Crud from "./components/Forms/CrudProductForm";
 import newCategory from "./components/Forms/NewCategoryForm";
--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Catalogue from "./Catalogo/Catalogue";
 import Footer from "./Footer";
 import style from "../CSS/homepage.module.scss";
 

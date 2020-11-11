2f0788ee2fd31dd42fe9d7f8e0ba70f8a7ac7642
Santiago Riveros <santiagoriverosjasis@gmail.com>
2020-11-11 11:27:43 -0300

    charlas raras

--- client/src/Components/Admin/adminProduct.jsx
+++ client/src/Components/Admin/adminProduct.jsx
@@ -1,3 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-export default function adminProduct({ setProducto }) {}
+export default function adminProduct({ setProducto }) {
+  const [productos, setProductos] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3000/products").then(({ data }) => {
+      setProductos(data);
+    });
+  }, []);
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>Nombre:</th>
+          <th>Descripcion:</th>
+          <th>Precio:</th>
+          <th>Stock:</th>
+          <th>+Agregar:</th>
+        </tr>
+        {productos.length &&
+          productos.map((product) => (
+            <tr>
+              <th>{product.title}</th>
+              <th>{product.description}</th>
+              <th>{product.price}</th>
+              <th>{product.stock}</th>
+              <th>
+                <button />
+              </th>
+            </tr>
+          ))}
+      </table>
+    </div>
+  );
+}

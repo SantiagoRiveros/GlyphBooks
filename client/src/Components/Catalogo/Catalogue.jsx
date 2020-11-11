fddfce6fbf520fbda5b536f518d078358bcb04ff
Santiago Riveros <santiagoriverosjasis@gmail.com>
2020-11-11 17:20:27 -0300

    se rompio

--- client/src/Components/Catalogo/Catalogue.jsx
+++ client/src/Components/Catalogo/Catalogue.jsx
@@ -5,12 +5,64 @@ import Searchbar from "./searchBar";
 import { useHistory } from "react-router-dom";
 import axios from "axios";
 import style from "../../CSS/catalogue.module.scss";
+import ReactPaginate from "react-paginate";
+import "../../CSS/pagination.module.css";
 
 export default function Catalogue() {
   const { push } = useHistory();
+  this.state = {
+    offset: 0,
+    data: [],
+    perPage: 9,
+    currentPage: 0,
+  };
   const [productos, setProductos] = useState([]);
   const [category, setCategory] = useState("");
   const [display, setDisplay] = useState([]);
+
+  receivedData(() => {
+    axios.get(`http://localhost:3000/products`).then((res) => {
+      const data = res.data;
+      const slice = data.slice(
+        this.state.offset,
+        this.state.offset + this.state.perPage
+      );
+      const postData = slice.map((producto) => (
+        <React.Fragment>
+          <Producto
+            img={producto.img}
+            title={producto.title}
+            price={producto.price}
+            key={producto.id}
+            id={producto.id}
+            OnClick={() => push(`/productos/${producto.id}`)}
+            categories={producto.Categories}
+          />
+        </React.Fragment>
+      ));
+
+      this.setState({
+        pageCount: Math.ceil(data.length / this.state.perPage),
+
+        postData,
+      });
+    });
+  });
+
+  handlePageClick = (e) => {
+    const selectedPage = e.selected;
+    const offset = selectedPage * this.state.perPage;
+
+    this.setState(
+      {
+        currentPage: selectedPage,
+        offset: offset,
+      },
+      () => {
+        this.receivedData();
+      }
+    );
+  };
 
   useEffect(() => {
     axios
@@ -45,7 +97,7 @@ export default function Catalogue() {
       <Sidebar className={style.Sidebar} setCategory={setCategory} />
       <div className={style.Size}>
         <div className={style.Relleno}>
-            <Searchbar onSearch={onSearch} />
+          <Searchbar onSearch={onSearch} />
           <div className={style.Catalogue}>
             {display.length &&
               display.map((producto) => {
@@ -61,6 +113,22 @@ export default function Catalogue() {
                   />
                 );
               })}
+          </div>
+          <div>
+            {this.state.postData}
+            <ReactPaginate
+              previousLabel={"prev"}
+              nextLabel={"next"}
+              breakLabel={"..."}
+              breakClassName={"break-me"}
+              pageCount={this.state.pageCount}
+              marginPagesDisplayed={2}
+              pageRangeDisplayed={5}
+              onPageChange={this.handlePageClick}
+              containerClassName={"pagination"}
+              subContainerClassName={"pages pagination"}
+              activeClassName={"active"}
+            />
           </div>
         </div>
       </div>

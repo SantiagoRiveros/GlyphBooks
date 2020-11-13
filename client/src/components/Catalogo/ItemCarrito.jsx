f7d02b52e9d96f5b5c55bf8fbcb7b9d0440672b7
SantiagoRiveros <santiagoriverosjasis@gmail.com>
2020-11-13 19:47:35 -0300

    we are peter chen

--- client/src/components/Catalogo/ItemCarrito.jsx
+++ client/src/components/Catalogo/ItemCarrito.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import style from "../../CSS/carrito.module.css";
-import { removeFromCart } from "../../actions/actions"
+import { removeFromCart } from "../../actions/actions";
 
 export default function Carrito(props) {
   const open = props.cartShow ? style.sidebarOpen : style.sidebar;
@@ -12,13 +12,14 @@ export default function Carrito(props) {
           <li>{props.title}</li>
           <li>{props.price}</li>
           <li>
-          {count}
-          <button onClick={() => setCount(count - 1)}>-</button>
-          <button onClick={() => setCount(count + 1)}>+</button>
+            {count}
+            <button onClick={() => setCount(count - 1)}>-</button>
+            <button onClick={() => setCount(count + 1)}>+</button>
           </li>
-          <li>D</li>
+          <li>
+            <button onClick={() => removeFromCart()}>X</button>
+          </li>
         </ul>
-        <button onClick={() => removeFromCart()}>X</button>
       </div>
     </div>
   );

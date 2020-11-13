75ded4374a3edb0460ecf031e51baacde972aa4d
SantiagoRiveros <santiagoriverosjasis@gmail.com>
2020-11-13 11:33:36 -0300

    faltan algunas cosas

---
+++ client/src/Components/Admin/admSideBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+
+export default function admSideBar() {
+  const { push } = useHistory();
+  return (
+    <div>
+      <ul>
+        <li OnClick={() => push(`/admin/products`)}>Productos</li>
+        <li OnClick={() => push(`/admin/orders`)}> Ordenes</li>
+        <li OnClick={() => push(`/admin/users`)}>Usuarios</li>
+      </ul>
+    </div>
+  );
+}
--- client/src/Components/Admin/admin.jsx
+++ client/src/Components/Admin/admin.jsx
@@ -2,11 +2,14 @@ import React, { useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import Crud from "../Forms/CrudProductForm";
 import AdminProducts from "./adminProducts";
+import OrderTable from "./orderTable.jsx";
 
 export default function Admin() {
   const [producto, setProducto] = useState(null);
   return (
     <Switch>
+      <Route path="/admin/orders" render={() => <OrderTable />} />
+      <Route path="/admin/users" render={} />
       <Route
         path="/admin/products"
         render={() => <AdminProducts setProducto={setProducto} />}

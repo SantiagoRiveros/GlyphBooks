c955654406b4344295b83f4eece4f167cee40d17
SantiagoRiveros <santiagoriverosjasis@gmail.com>
2020-11-13 12:08:10 -0300

    me habia olvidado de renderizar F

--- client/src/Components/Admin/admin.jsx
+++ client/src/Components/Admin/admin.jsx
@@ -3,13 +3,14 @@ import { Switch, Route } from "react-router-dom";
 import Crud from "../Forms/CrudProductForm";
 import AdminProducts from "./adminProducts";
 import OrderTable from "./orderTable.jsx";
+import AdminUsers from "./adminUsers.jsx";
 
 export default function Admin() {
   const [producto, setProducto] = useState(null);
   return (
     <Switch>
       <Route path="/admin/orders" render={() => <OrderTable />} />
-      <Route path="/admin/users" render={} />
+      <Route path="/admin/users" render={() => <AdminUsers />} />
       <Route
         path="/admin/products"
         render={() => <AdminProducts setProducto={setProducto} />}

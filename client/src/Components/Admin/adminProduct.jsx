39b8ffcf8a18075171a9ab7045898035036d7269
SantiagoRiveros <santiagoriverosjasis@gmail.com>
2020-11-13 15:36:26 -0300

    oh mama ella me ha besado

--- client/src/Components/Admin/adminProduct.jsx
+++ client/src/Components/Admin/adminProduct.jsx
@@ -1,22 +1,43 @@
 import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 import axios from "axios";
+import style from "../../CSS/catalogue.module.scss";
+import Pagination from "../Catalogo/pagination.jsx";
 
 export default function AdminProduct({ setProducto }) {
   const [productos, setProductos] = useState([]);
+  const { push } = useHistory();
   useEffect(() => {
     axios.get("http://localhost:3000/products").then(({ data }) => {
       setProductos(data);
     });
   }, []);
+
   return (
     <div>
+      <div className={style.Btns}>
+        <button
+          className={style.Button}
+          name="crud"
+          onClick={() => push("/crud")}
+        >
+          NUEVO PRODUCTO
+        </button>
+        <button
+          className={style.Button}
+          name="newCategory"
+          onClick={() => push("/newCategory")}
+        >
+          NUEVA CATEGORÍA
+        </button>
+      </div>
       <table>
         <tr>
           <th>Nombre:</th>
           <th>Descripcion:</th>
           <th>Precio:</th>
           <th>Stock:</th>
-          <th>+Agregar:</th>
+          <th>Editar:</th>
         </tr>
         {productos.length &&
           productos.map((product) => (
@@ -26,11 +47,12 @@ export default function AdminProduct({ setProducto }) {
               <th>{product.price}</th>
               <th>{product.stock}</th>
               <th>
-                <button />
+                <button>+</button>
               </th>
             </tr>
           ))}
       </table>
+      <Pagination />
     </div>
   );
 }

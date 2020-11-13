8923fa23381c6fcdafbf48258c4ffa45be0e6fc8
SantiagoRiveros <santiagoriverosjasis@gmail.com>
2020-11-13 12:06:33 -0300

    esta la interfaz

---
+++ client/src/Components/Admin/adminUsers.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function adminUsers() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3000/users").then(({ data }) => {
+      setUsers(data);
+    });
+  }, []);
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>ID:</th>
+          <th>Nombre:</th>
+          <th>Email:</th>
+          <th>Rol:</th>
+          <th>Detalles:</th>
+        </tr>
+        {users.length &&
+          users.map((user) => (
+            <tr>
+              <th>{user.id}</th>
+              <th>{user.firstName + " " + user.Lastname}</th>
+              <th>{user.email}</th>
+              <th>{user.isAdmin ? "Admin" : "User"}</th>
+              <th>
+                <button />
+              </th>
+            </tr>
+          ))}
+      </table>
+    </div>
+  );
+}

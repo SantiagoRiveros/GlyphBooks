cfb74e283e7d00fdccba26aaecd368a19b54ec80
Mauricio Ocando <mauroocando@gmail.com>
2020-11-12 21:02:15 -0300

    cart reducer

--- client/src/reducers/cart.reducer.js
+++ client/src/reducers/cart.reducer.js
@@ -1,22 +1,22 @@
 import { ADD_TO_CART, REMOVE_FROM_CART } from "../constants/cart.constants";
 
 const initialState = {
-  productos: []
-}
+  productos: [],
+};
 
 function cartReducer(state = initialState, action) {
   switch (action.type) {
     case ADD_TO_CART: {
       return {
         ...state,
-        productos:
-      }
+        productos: [...state.productos, action.producto],
+      };
     }
     case REMOVE_FROM_CART: {
       return {
         ...state,
-        productos:
-      }
+        productos: state.productos.filter((e) => !e.id === action.id),
+      };
     }
   }
 }

734a6b07b91f48a2efd560b29e2c9b6b6a820d6b
SantiagoRiveros <santiagoriverosjasis@gmail.com>
2020-11-14 12:03:09 -0300

    peter chen its my life

--- client/src/components/Catalogo/searchBar.jsx
+++ client/src/components/Catalogo/searchBar.jsx
@@ -1,16 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import style from "../../CSS/searchBar.module.scss";
+import axios from "axios";
 
 export default function SearchBar({ onSearch }) {
   const [book, setBook] = useState("");
+
   return (
-    <form className={style.form}
+    <form
+      className={style.form}
       onSubmit={(e) => {
         e.preventDefault();
         onSearch(book);
       }}
     >
-      <input className={style.input}
+      <input
+        className={style.input}
         type="text"
         placeholder="Autor, título, descripción..."
         value={book}

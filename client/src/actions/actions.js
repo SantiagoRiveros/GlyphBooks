85d89d118da9ec2e24b4dd479ee3e993bec6adbc
ona <ona.lore5@gmail.com>
2020-11-15 00:22:52 -0300

    implementar redux

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -1,5 +1,5 @@
 import { LOGIN, LOGOUT } from "../constants/user.constants";
-import { ADD_TO_CART, REMOVE_FROM_CART, AGREGAR_CARRITO, REMOVER_CARRITO } from "../constants/cart.constants";
+import { AGREGAR_CARRITO, REMOVER_CARRITO } from "../constants/cart.constants";
 
 
 export function login(user) {

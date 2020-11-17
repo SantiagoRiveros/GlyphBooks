d058bb06e33be9109f3fc914f7e0de7b1efd5987
Sergio Pepe <sergio_pp95@hotmail.com>
2020-11-17 12:43:13 -0300

    Authenticate Passport

---
+++ api/src/passport.js
@@ -0,0 +1,106 @@
+const jwt = require("jsonwebtoken"),
+{ User } = require("./db"),
+passport = require("passport"),
+LocalStrategy = require("passport-local").Strategy,
+BearerStrategy = require("passport-http-bearer").Strategy;
+
+const SECRET = process.env.AUTH_SECRET || "secret"
+
+
+passport.use(
+new LocalStrategy(
+  { usernameField: "email", passwordField: "password", session: false },
+  async (email, password, done) => {
+    const user = await User.findOne({where: {
+      email
+    }});
+    if (!user)
+      return done(null, false, {
+        message: "Username or password is incorrect",
+      });
+    if (!user.compare(password))
+      return done(null, false, {
+        message: "Username or password is incorrect",
+      });
+    return done(null, user);
+  }
+)
+);
+// passport.use(
+// new GoogleStrategy(
+//   {
+//     clientID: GOOGLE_CLIENT_ID,
+//     clientSecret: GOOGLE_CLIENT_SECRET,
+//     callbackURL: "/auth/login/google/callback",
+//     session: false,
+//   },
+//   async (token, tokenSecret, profile, done) => {
+//     let user = await getOneByGoogleId(profile.id);
+//     if (!user)
+//       user = await createOne(
+//         profile.displayName,
+//         profile.emails[0].value,
+//         null,
+//         "GUEST",
+//         profile.id,
+//         null
+//       );
+//     const { id, name, email, role, status, createdAt, updatedAt } = user;
+//     return done(null, {
+//       id,
+//       name,
+//       email,
+//       role,
+//       status,
+//       createdAt,
+//       updatedAt,
+//     });
+//   }
+// )
+// );
+
+// passport.use(
+// new FacebookStrategy(
+//   {
+//     clientID: FACEBOOK_APP_ID,
+//     clientSecret: FACEBOOK_APP_SECRET,
+//     callbackURL: "/auth/login/facebook/callback",
+//     profileFields: ["id", "emails", "displayName"],
+//   },
+//   async function (accessToken, refreshToken, profile, done) {
+//     console.log(profile);
+//     let user = await getOneByFacebookId(profile.id);
+//     if (!user)
+//       user = await createOne(
+//         profile.displayName,
+//         profile.emails,
+//         null,
+//         "GUEST",
+//         null,
+//         profile.id
+//       );
+//     const { id, name, email, role, status, createdAt, updatedAt } = user;
+//     return done(null, {
+//       id,
+//       name,
+//       email,
+//       role,
+//       status,
+//       createdAt,
+//       updatedAt,
+//     });
+//   }
+// )
+// );
+
+passport.use(
+new BearerStrategy((token, done) => {
+  jwt.verify(token, SECRET, function (err, user) {
+    if (err) return done(err);
+    return done(null, user ? user : false);
+  });
+})
+);
+
+
+module.exports = passport;
---
+++ api/src/routes/userRegist.js
@@ -0,0 +1,34 @@
+const autenticate = require("express").Router();
+const { Product, Category, User, Order } = require("../db.js");
+const { Op } = require("sequelize");
+const passport = require('passport')
+const jwt = require('jsonwebtoken')
+
+
+
+
+autenticate.post('/register', (req, res, next) => {
+    const users = req.body;
+
+    User.create({ ...users})
+      .then(() => res.sendStatus(201))
+      .catch(next);
+});
+
+
+
+autenticate.post('/login', (req, res, next) => {
+    passport.authenticate('local', function(err, user, info) {
+        if(err) return next(err);
+        if(!user) {
+            return res.sendStatus(401)
+        } else {
+            return res.json( {
+                user,
+                token: jwt.sign(user, SECRET)
+            })
+        }
+    })(req,res,next);
+});
+
+module.exports= autenticate;

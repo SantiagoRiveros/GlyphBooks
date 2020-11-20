ef3244508cd3bb34156b130754966e0327a311d7
ona <ona.lore5@gmail.com>
2020-11-20 15:04:20 -0300

    ruta over

--- client/src/components/Admin/admin.jsx
+++ client/src/components/Admin/admin.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { Switch, Route, useHistory } from "react-router-dom";
 import Crud from "../Forms/CrudProductForm";
 import NewCategory from "../Forms/NewCategoryForm";
 import AdminProduct from "./adminProduct";
@@ -14,27 +15,34 @@ import style from "../../CSS/Admin/admin.module.scss";
 export default function Admin() {
   const [producto, setProducto] = useState(null);
   const [review, setReview] = useState(null);
+  const user = useSelector(state => state.user);
+  const { replace } = useHistory();
+  useEffect(() => {
+    if(!user.user?.isAdmin) replace("/")
+  }, [])
   return (
     <div className={style.fondo}>
-      <AdmSideBar />
-      <div className={style.relleno}>
-        <Switch>
-          <Route path="/admin/orders" render={() => <OrderTable />} />
-          <Route path="/admin/users" render={() => <AdminUsers />} />
-          <Route path="/admin/reviews" render={() => <AdminReviews />} />
-          <Route path="/admin/crud" render={() => <ProductCrud />} />
-          <Route path="/admin/orderDetails" render={() => <OrderDetails />} />
-          <Route
-            path="/admin/products"
-            render={() => <AdminProduct setProducto={setProducto} />}
-          />
-          <Route
-            path="/admin/crud"
-            render={() => <Crud product={producto} setProduct={setProducto} />}
-          />
-          <Route path="/admin/newCategory" render={() => <NewCategory />} />
-        </Switch>
-      </div>
+      {user.user?.isAdmin?<div>
+        <AdmSideBar />
+        <div className={style.relleno}>
+          <Switch>
+            <Route path="/admin/orders" render={() => <OrderTable />} />
+            <Route path="/admin/users" render={() => <AdminUsers />} />
+            <Route path="/admin/reviews" render={() => <AdminReviews />} />
+            <Route path="/admin/crud" render={() => <ProductCrud />} />
+            <Route path="/admin/orderDetails" render={() => <OrderDetails />} />
+            <Route
+              path="/admin/products"
+              render={() => <AdminProduct setProducto={setProducto} />}
+            />
+            <Route
+              path="/admin/crud"
+              render={() => <Crud product={producto} setProduct={setProducto} />}
+            />
+            <Route path="/admin/newCategory" render={() => <NewCategory />} />
+          </Switch>
+        </div>
+      </div>:null}
     </div>
   );
 }
